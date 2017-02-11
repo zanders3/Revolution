277e4428a1f098803d89ae6e9d78fd19d5dea68d
Alex Parker <3zanders@gmail.com>
2017-02-11 13:07:31 +0000

    Add Orestes and tank AI started

--- Assets/Code/Tank.cs
+++ Assets/Code/Tank.cs
@@ -1,11 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
-
-public enum TankState
-{
-    Moving,
-    Attacking
-}
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Tank : MonoBehaviour
@@ -16,17 +11,64 @@ public class Tank : MonoBehaviour
     public Material[] BlueMaterials;
 
     public float AttackRadius = 10f;
+    public Transform TurretTransform;
+
+    Vector3 factoryTarget;
 
 	public void Setup(Vector3 target, Team team)
     {
+        Team = team;
+        factoryTarget = target;
         GetComponentInChildren<SkinnedMeshRenderer>().sharedMaterials = team == Team.Red ? RedMaterials : BlueMaterials;
 
         agent = GetComponent<NavMeshAgent>();
         agent.destination = target;
+        StartCoroutine(RunAI());
     }
 
-    void Update()
+    void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, AttackRadius);
+    }
+
+    IEnumerator RunAI()
+    {
+        while (true)
+        {
+            //Pick the best target
+            Tank target = null;
+            Factory targetFactory = null;
+            while (target == null && targetFactory == null)
+            {
+                Collider[] colliders = Physics.OverlapSphere(transform.position, AttackRadius);
+                float bestTargetDist = float.MaxValue;
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    Tank tank = colliders[i].GetComponent<Tank>();
+                    if (tank != null && tank.Team != Team)
+                    {
+                        float dist = Vector3.Distance(transform.position, tank.transform.position);
+                        if (dist < bestTargetDist)
+                        {
+                            target = tank;
+                            bestTargetDist = dist;
+                        }
+                    }
+                    Factory factory = colliders[i].GetComponent<Factory>();
+                    if (factory != null)
+                    {
+                        targetFactory = factory;
+                    }
+                }
+                yield return null;
+            }
+
+            agent.Stop();
+
+            Vector3 targetPosition = targetFactory != null ? targetFactory.transform.position : target.transform.position;
+            Vector3 targetDelta = (targetPosition - transform.position).normalized;
+            TurretTransform.rotation = Quaternion.LookRotation(-targetDelta, Vector3.up);
+        }
     }
 }
