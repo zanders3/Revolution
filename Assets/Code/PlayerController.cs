9e830b16b50addbef18cc199df8aa9b8ca19ed0d
Hale <event004@ecs.soton.ac.uk>
2017-02-11 10:45:56 +0000

    Added player input for switching active factory

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Factory[] FactoryList;
+    int SelectedFactoryIdx = -1;
+    public GameObject SelectionIndicator;
+
+    public string UpInputString;
+    public string DownInputString;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ChangeSelection(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButtonDown(UpInputString))
+        {
+            ChangeSelection(SelectedFactoryIdx - 1);
+        }
+        else if (Input.GetButtonDown(DownInputString))
+        {
+            ChangeSelection(SelectedFactoryIdx + 1);
+        }
+    }
+
+    void ChangeSelection(int NewSelectionIdx)
+    {
+        // Don't do anything if we try to change to the currently selected factory
+        if (NewSelectionIdx == SelectedFactoryIdx)
+        {
+            return;
+        }
+
+        // Don't do anything if the new index is invalid
+        if (NewSelectionIdx < 0 || NewSelectionIdx >= FactoryList.Length)
+        {
+            return;
+        }
+
+        // Deselect the currently selected factory (if it is valid)
+        if (SelectedFactoryIdx >= 0 && SelectedFactoryIdx < FactoryList.Length)
+        {
+            FactoryList[SelectedFactoryIdx].bIsActive = false;
+        }
+
+        // Select the new factory
+        SelectedFactoryIdx = NewSelectionIdx;
+        FactoryList[SelectedFactoryIdx].bIsActive = true;
+        SelectionIndicator.transform.position = FactoryList[SelectedFactoryIdx].transform.position;
+    }
+}
