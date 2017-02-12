745ac731b49373245ceac4dc7571028049fe6132
Hale <haleharding@gmail.com>
2017-02-12 11:19:13 +0000

    Setup for the UI to support multiple units

---
+++ Assets/Code/UnitIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitIcon : MonoBehaviour
+{
+    public Image Icon;
+    public Image Overlay;
+    public Text Cost;
+    public Text Key;
+}
